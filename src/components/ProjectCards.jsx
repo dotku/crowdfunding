7db00a906daa33635673701260bcbffcb92f609b
jay-itl <weijingjaylin@gmail.com>
2024-08-03 14:20:10 -0700

    add tailswind pacakges

--- src/components/ProjectCards.jsx
+++ src/components/ProjectCards.jsx
@@ -3,15 +3,19 @@ import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/react";
 
 export default function ProjectCards() {
   console.log("Projects", Projects);
-  return Array.isArray(Projects) && Projects.length
-    ? Projects.map((p) => (
-        <Card key={p.id}>
-          <CardHeader>{p.name}</CardHeader>
-          <CardBody>{p.discription}</CardBody>
-          <CardFooter>
-            <a href={`/project/${p.id}`}>More</a>
-          </CardFooter>
-        </Card>
-      ))
-    : null;
+  return (
+    <div className="flex justify-between">
+      {Array.isArray(Projects) && Projects.length
+        ? Projects.map((p) => (
+            <Card key={p.id} className="w-[30%]">
+              <CardHeader>{p.name}</CardHeader>
+              <CardBody>{p.discription}</CardBody>
+              <CardFooter>
+                <a href={`/project/${p.id}`}>More</a>
+              </CardFooter>
+            </Card>
+          ))
+        : null}
+    </div>
+  );
 }
