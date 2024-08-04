b4795d5d1b06590c45e6ef70c2af753d9d694a36
jay-itl <weijingjaylin@gmail.com>
2024-08-03 18:28:22 -0700

    commit bugy crowdfunding

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import ProjectCards from "./components/ProjectCards";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <ProjectCards />
+      <h1 className="text-2xl font-bold underline">Hello world!</h1>
     </>
   );
 }
